65943fd9aaba76197376820946247726d6304fd4
Martin Lauridsen <martinlauridsen_93@hotmail.com>
2024-07-18 23:28:36 +0200

    optimize code and linting

--- app/src/main/java/com/example/randomdog/ui/main/FavouriteDogsFragment.kt
+++ app/src/main/java/com/example/randomdog/ui/main/FavouriteDogsFragment.kt
@@ -1,22 +1,18 @@
 package com.example.randomdog.ui.main
 
-import android.app.Activity
-import android.content.Context
 import android.content.res.Configuration
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.randomdog.R
 import com.example.randomdog.databinding.FragmentFavouriteDogsBinding
-import com.example.randomdog.databinding.FragmentRandomDogBinding
 import com.example.randomdog.model.Dog
 import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
@@ -28,31 +24,33 @@ class FavouriteDogsFragment : Fragment() {
 
     private lateinit var viewModel: FavouriteDogsViewModel
 
-    private var coloumns = 0;
+    private var columns = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        coloumns = if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) 3 else 2
+        columns =
+            if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) 3 else 2
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_favourite_dogs, container, false)
-        viewModel = ViewModelProvider(this).get(FavouriteDogsViewModel::class.java)
+    ): View {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_favourite_dogs, container, false)
+        viewModel = ViewModelProvider(this)[FavouriteDogsViewModel::class.java]
 
         val view = binding.root
 
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
 
-        var recyclerView = view.findViewById<RecyclerView>(R.id.favouritesRecyclerView)
-        recyclerView.layoutManager = GridLayoutManager(this.context, coloumns)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.favouritesRecyclerView)
+        recyclerView.layoutManager = GridLayoutManager(this.context, columns)
 
-        viewModel.favouriteDogsList.observe(viewLifecycleOwner){
-            if(recyclerView.adapter == null){
-              recyclerView.adapter = FavouritesAdapter()
+        viewModel.favouriteDogsList.observe(viewLifecycleOwner) {
+            if (recyclerView.adapter == null) {
+                recyclerView.adapter = FavouritesAdapter()
             }
             (recyclerView.adapter as FavouritesAdapter).updateList(it)
         }
@@ -65,19 +63,19 @@ class FavouriteDogsFragment : Fragment() {
         viewModel.FetchFavouriteDogs()
     }
 
-    class FavouritesAdapter(): RecyclerView.Adapter<FavouritesAdapter.ViewHolder>(){
+    class FavouritesAdapter : RecyclerView.Adapter<FavouritesAdapter.ViewHolder>() {
 
-        var dataList = emptyList<Dog>()
+        private var dataList = emptyList<Dog>()
 
         init {
             setData(dataList)
         }
 
-        fun setData(dataList: List<Dog>){
+        private fun setData(dataList: List<Dog>) {
             this.dataList = dataList
         }
 
-        class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
             var view: ImageView
 
             init {
@@ -86,13 +84,14 @@ class FavouriteDogsFragment : Fragment() {
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            var view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_favourite_dog, parent, false)
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.list_item_favourite_dog, parent, false)
             return ViewHolder(view)
         }
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
             // Get the data model based on position
-            var dog = dataList[position]
+            val dog = dataList[position]
 
             // Set item views based on your views and data model
             Picasso.get().load(dog.url).into(holder.view)
@@ -102,7 +101,7 @@ class FavouriteDogsFragment : Fragment() {
             return dataList.size
         }
 
-        public fun updateList(dataList: List<Dog>){
+        fun updateList(dataList: List<Dog>) {
             this.dataList = dataList
             notifyDataSetChanged()
         }
